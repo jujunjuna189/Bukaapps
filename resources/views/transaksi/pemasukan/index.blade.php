470b9bd813933763efce3fec45dd5d7ff8de65ea
Fanny Fietya <fnyfh10@gmail.com>
2022-04-10 22:42:04 +0700

    Update index.blade.php

--- resources/views/transaksi/pemasukan/index.blade.php
+++ resources/views/transaksi/pemasukan/index.blade.php
@@ -1,4 +1,27 @@
 @extends('layouts.app_templates')
 @section('content')
+<div class="media-body">
+    <div class="az-content-header">
+        <div>
+            <h6 class="az-content-title tx-18 mg-b-5">Pemasukan</h6>
+            <p class="az-content-text tx-13 mg-b-0">Hi, welcome back! Here's your summary of your events.</p>
+        </div>
+        <div class="float-right text-right">
+            <div class="row row-sm">
+                <div class="col-lg-4">
+                    <div class="btn-group" role="group" aria-label="Basic example">
+                            <button type="button" class="btn btn-secondary pd-x-25 active">Pemasukan</button>
+                            <button type="button" class="btn btn-outline-secondary pd-x-25">Pengeluaran</button>
+                    </div>
+                </div><!-- col-4 -->
+            </div>
+        </div>
+    </div><!-- az-content-header -->
 
+    <div class="card">
+        <div class="card-body">
+            
+        </div>
+    </div>
+</div><!-- media-body -->
 @endsection
