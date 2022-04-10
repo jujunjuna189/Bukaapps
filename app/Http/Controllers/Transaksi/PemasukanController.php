f8a510c562594306bce854509eb551a18ff264f3
jujunjuna189 <53271616+jujunjuna189@users.noreply.github.com>
2022-04-10 23:57:30 +0700

    Update Pemasukan

--- app/Http/Controllers/Transaksi/PemasukanController.php
+++ app/Http/Controllers/Transaksi/PemasukanController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Transaksi;
 
 use App\Http\Controllers\Controller;
+use App\Models\Pemasukan;
 use Illuminate\Http\Request;
 
 class PemasukanController extends Controller
 {
-    public function index(){
-        return view('transaksi.pemasukan.index');
+    public function index()
+    {
+
+        $data['pemasukan'] = Pemasukan::all();
+        return view('transaksi.pemasukan.index', $data);
     }
 }
--- resources/views/transaksi/pemasukan/index.blade.php
+++ resources/views/transaksi/pemasukan/index.blade.php
@@ -1,32 +1,53 @@
 @extends('layouts.app_templates')
 @section('content')
 <div class="media-body">
-    <div class="az-content-header">
-        <div>
+    <div class="az-content-header row">
+        <div class="col-lg-6">
             <h6 class="az-content-title tx-18 mg-b-5">Pemasukan</h6>
-            <p class="az-content-text tx-13 mg-b-0">Hi, welcome back! Here's your summary of your events.</p>
+            <p class="az-content-text tx-13 mg-b-0 mb-lg-0 mb-3">Hi, welcome back! Here's your summary of your events.</p>
         </div>
-        <div class="float-right text-right">
-            <div class="row row-sm">
-                <div class="col-lg-4">
-                    <div class="btn-group" role="group" aria-label="Basic example">
-                        <button type="button" class="btn btn-secondary pd-x-25 active">Pemasukan</button>
-                        <a href="{{route('transaksi.pengeluaran')}}" class="btn btn-outline-secondary pd-x-25">Pengeluaran</a>
-                    </div>
-                </div><!-- col-4 -->
+        <div class="col-lg-6">
+            <div class="float-right">
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <button type="button" class="btn btn-secondary pd-x-25 active">Pemasukan</button>
+                    <a href="{{route('transaksi.pengeluaran')}}" class="btn btn-outline-secondary pd-x-25">Pengeluaran</a>
+                </div>
             </div>
         </div>
     </div><!-- az-content-header -->
 
     <div class="card">
         <div class="card-body">
-            <div class="row">
-                <div class="col-lg-6">
+            <div class="nav-scroller">
+                <ul class="nav nav-tabs tickets-tab-switch" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link rounded active" id="open-tab" data-bs-toggle="tab" href="#open-tickets" role="tab" aria-controls="open-tickets" aria-selected="true">Pemasukan <div class="badge">13</div></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link rounded" id="pending-tab" data-bs-toggle="tab" href="#pending-tickets" role="tab" aria-controls="pending-tickets" aria-selected="false">Pengeluaran <div class="badge">50 </div></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="mt-4">
+                <span class="text-muted"><i class="typcn typcn-calendar-outline"></i> Tanggal Last Update</span>
+            </div>
 
+            <div class="mt-4">
+                @foreach($pemasukan as $val)
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h5>{{ $val->title }}</h5>
+                                <p class="mb-0"><small>{{ $val->created_at }}</small></p>
+                            </div>
+                            <div class="col-lg-6 text-right">
+                                <h6 class="text-primary">{{ $val->total }}</h6>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-lg-6">
-
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
