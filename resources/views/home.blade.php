52416c33d90c2e1fe676bdae07024d2c6ea6ddc8
jujunjuna189 <53271616+jujunjuna189@users.noreply.github.com>
2022-04-13 02:01:20 +0700

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,9 @@
 @section('content')
 <div class="media-body">
     <div class="row row-sm">
-        <div class="col-sm-12 col-md-12 mg-t-20 mg-md-t-0">
-            <div class="az-content-label mg-b-5">Area Chart</div>
-            <p class="mg-b-20">Below is the basic area chart example.</p>
-            <div class="chartjs-wrapper-demo"><canvas id="chartArea1"></canvas></div>
+        <div class="col-sm-12 col-md-12 col-xl-12 mg-t-20 mg-md-t-0">
+            <div class="az-content-label az-content-label-sm mg-b-15">With Transparency</div>
+            <div class="ht-200 ht-lg-250"><canvas id="chartBar2"></canvas></div>
         </div><!-- col-6 -->
     </div><!-- row -->
 </div>
@@ -16,35 +15,21 @@
     $(function() {
         'use strict';
 
-        /** AREA CHART **/
-        var ctx9 = document.getElementById('chartArea1');
 
-        var gradient1 = ctx3.createLinearGradient(0, 350, 0, 0);
-        gradient1.addColorStop(0, 'rgba(241,0,117,0)');
-        gradient1.addColorStop(1, 'rgba(241,0,117,.5)');
-
-        var gradient2 = ctx3.createLinearGradient(0, 280, 0, 0);
-        gradient2.addColorStop(0, 'rgba(0,123,255,0)');
-        gradient2.addColorStop(1, 'rgba(0,123,255,.3)');
-
-        new Chart(ctx9, {
-            type: 'line',
+        var ctx2 = document.getElementById('chartBar2').getContext('2d');
+        new Chart(ctx2, {
+            type: 'bar',
             data: {
-                labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
                 datasets: [{
-                    data: [12, 15, 18, 40, 35, 38, 32, 20, 25, 15, 25, 30],
-                    borderColor: '#f10075',
-                    borderWidth: 1,
-                    backgroundColor: gradient1
-                }, {
-                    data: [10, 20, 25, 55, 50, 45, 35, 37, 45, 35, 55, 40],
-                    borderColor: '#007bff',
-                    borderWidth: 1,
-                    backgroundColor: gradient2
+                    label: '# of Votes',
+                    data: [12, 39, 20, 10, 25, 18],
+                    backgroundColor: 'rgba(0,123,255,.5)'
                 }]
             },
             options: {
                 maintainAspectRatio: false,
+                responsive: true,
                 legend: {
                     display: false,
                     labels: {
@@ -60,6 +45,7 @@
                         }
                     }],
                     xAxes: [{
+                        barPercentage: 0.6,
                         ticks: {
                             beginAtZero: true,
                             fontSize: 11
@@ -68,6 +54,7 @@
                 }
             }
         });
+
     });
 </script>
 @endsection
